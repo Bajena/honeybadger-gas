0e243c9a317c16e5d5a184a1d1db7b0502a88aed
Jan Bajena <bajena3@gmail.com>
2018-10-11 23:19:34 +0200

    Extract Honeybadger class to a file and add readme + license

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# gas-clasp-starter
-A starter template for Google Apps Script by [google/clasp](https://github.com/google/clasp)
-
-## Article
-[(Japanese) Google Apps Script をローカル環境で快適に開発するためのテンプレートを作りました](https://qiita.com/howdy39/items/0e799a9bfc1d3bccf6e5)
+# Honeybadger GAS
+A Honeybadger client for Google Apps Scripts.
 
 ## Tech Stack
 - [google/clasp](https://github.com/google/clasp)
@@ -19,13 +16,14 @@ A starter template for Google Apps Script by [google/clasp](https://github.com/g
 ## Getting Started
 ### Clone the repository
 ```
-git clone --depth=1 https://github.com/howdy39/gas-clasp-starter.git <project_name>
+git clone https://github.com/Bajena/honeybadger-gas.git <project_name>
 ```
 
 ### Install dependencies
 ```
 cd <project_name>
 npm install
+yarn
 ```
 
 ### Configuration
@@ -38,17 +36,6 @@ What is scriptId ? https://github.com/google/clasp#scriptid-required
 }
 ```
 
-#### Open `src/appsscript.json`, change timeZone (optional)
-[Apps Script Manifests](https://developers.google.com/apps-script/concepts/manifests)
-```
-{
-  "timeZone": "Asia/Tokyo", ## Change timeZone
-  "dependencies": {
-  },
-  "exceptionLogging": "STACKDRIVER"
-}
-```
-
 ### Development and build project
 ```
 npm run build
@@ -58,13 +45,6 @@ npm run build
 ```
 clasp push
 ```
-
-## Others
-### howdy39/gas-clasp-library
-[howdy39/gas-clasp-library](https://github.com/howdy39/gas-clasp-library) is sample project made with [Google Apps Script Libraries](https://developers.google.com/apps-script/guides/libraries).   
-also, `gas-clasp-library` use circle CI.
-
-
 
 ## License
 This software is released under the MIT License, see LICENSE.txt.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { StacktraceParser, StacktraceEntry } from './stacktrace-parser';
-import { ErrorData } from './error-data';
+import { Honeybadger } from './honeybadger';
 
 declare var global: any;
 
@@ -15,93 +14,5 @@ global.notify = (): void => {
 global.throwError = (): void => {
   throw new Error('xxx');
 };
-
-class Honeybadger {
-  apiKey: string;
-  context: Object;
-
-  constructor(apiKey: string) {
-    this.apiKey = apiKey;
-  }
-
-  notify(error: any): void {
-    const ed = new ErrorData(error);
-    const payload = this.buildPayload(ed);
-    const options = {
-      contentType: 'application/json',
-      payload: JSON.stringify(payload),
-      headers: {
-        'X-API-Key': this.apiKey
-      }
-    };
-    options['method'] = 'post';
-
-    console.log('Posting with options', options);
-    console.log('Payload', payload);
-    const resp = UrlFetchApp.fetch('https://api.honeybadger.io/v1/notices', options);
-    console.log(resp);
-  }
-
-  private buildPayload(errorData: ErrorData): Object {
-    const backtrace = this.buildBacktrace(errorData);
-
-    return {
-      // Please use relevant values for the name, url, and version keys of the
-      // `notifier` key that point back to your code/package.
-      notifier: {
-        name: 'Honeybadger GAS Notifier',
-        url: 'https://github.com/Bajena/honeybadger-gas',
-        version: '0.0.1'
-      },
-      // Here's where you're exception's class, message tags and backtrace go.
-      // The `class` and `message` attributes are what make up the error's "title" that we display in the UI.
-      // The `fingerprint` attribute is an optional string that is used to force errors with the same fingerprint (regardless of error class, message, or location) to be grouped together.
-      // The `backtrace` is a ruby-style backtrace.  If the `source` attribute is included for a backtrace line, it will be displayed as a snippet in the UI,
-      // Last but not least, `causes` is an optional list of causes for the error.
-      // Honeybadger displays causes in the order they are listed here.
-      error: {
-        class: errorData.name,
-        message: errorData.message,
-        backtrace: backtrace
-      },
-
-      // `request` contains information about the HTTP request that caused this exception.
-      request: {
-        // We display this data on the error's details page.
-        // `user_id` and `user_email` are special keys that are used to generate the "affected users" list in the UI.
-        context: {
-          user_id: 123,
-          user_email: 'test@example.com'
-        },
-
-        // These are displayed under "params" on the error detail page
-        params: {
-          // TODO: tutaj parametry wywolania skryptu
-        }
-      },
-      server: {
-        // Your environment name
-        environment_name: 'development', // TODO: tutaj nazwa deploymentu
-
-        // Optional: Git sha for the deployed version of the code, for linking to GitHub, Gitlab, and BitBucket
-        revision: '920201a', // TODO: Tutaj numer wersji
-
-        // Optional: ID of the process that raised the error
-        pid: 1138 // TODO: Tutaj wstaw execution id
-      }
-    };
-  }
-
-  private buildBacktrace(errorData: ErrorData): Object[] {
-    console.log('Building backtrace for object', errorData);
-    return new StacktraceParser(errorData.stack).parse().map(item => {
-      return {
-        number: item.lineNumber,
-        method: item.functionName,
-        file: item.fileName
-      };
-    });
-  }
-}
 
 global.Honeybadger = Honeybadger;
